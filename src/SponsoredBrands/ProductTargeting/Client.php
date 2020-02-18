7fd8424b9843ef7ecfe3821b8f7e2f711c911568
baihe <baihe@guahao.com>
2020-02-18 16:05:29 +0800

    完善商品投放相关 接口

---
+++ src/SponsoredBrands/ProductTargeting/Client.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace easyAmazonAdv\SponsoredBrands\ProductTargeting;
+
+use easyAmazonAdv\Kernel\BaseClient;
+
+/**
+ * Class Client
+ * @package easyAmazonAdv\SponsoredBrands\ProductTargeting
+ *
+ * @author  baihe <b_aihe@163.com>
+ * @date    2020-02-18 15:37
+ */
+class Client extends BaseClient
+{
+    /**
+     * listTargeting
+     * @param array $params
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 15:38
+     */
+    public function listTargetingClauses(array $params)
+    {
+        return $this->httpPost('/sb/targets/list', $params, [], false);
+    }
+
+    /**
+     * updateTargetingClauses.
+     *
+     * @param array $params
+     *
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2019-11-14 19:50
+     */
+    public function updateTargetingClauses(array $params)
+    {
+        return $this->httpPut('/sb/targets', $params, [], false);
+    }
+
+    /**
+     * createTargetingClauses
+     *
+     * @param array $params
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:03
+     */
+    public function createTargetingClauses(array $params)
+    {
+        return $this->httpPost('/sb/targets', $params, [], false);
+    }
+
+    /**
+     * getTargetingClause
+     *
+     * @param string $targetId
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 15:46
+     */
+    public function getTargetingClause(string $targetId)
+    {
+        return $this->httpGet('/sb/targets/' . $targetId, [], false);
+    }
+
+    /**
+     * archiveTargetingClauses
+     *
+     * @param string $targetId
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 15:47
+     */
+    public function archiveTargetingClauses(string $targetId)
+    {
+        return $this->httpDelete('/sb/targets/' . $targetId, [], [], false);
+    }
+
+    /**
+     * batchGetTargetingClauses
+     *
+     * @param array $targetIds
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 15:49
+     */
+    public function batchGetTargetingClauses(array $targetIds)
+    {
+        return $this->httpPost('/sb/targets/batchGet', ['targetIds' => $targetIds], [], false);
+    }
+
+    /**
+     * listNegativeTargetingClauses
+     * @param array $params
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:01
+     */
+    public function listNegativeTargetingClauses(array $params)
+    {
+        return $this->httpPost('/sb/negativeTargets/list', $params, [], false);
+    }
+
+    /**
+     * updateNegativeTargetingClauses
+     *
+     * @param array $params
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:01
+     */
+    public function updateNegativeTargetingClauses(array $params)
+    {
+        return $this->httpPut('/sb/negativeTargets', $params, [], false);
+    }
+
+    /**
+     * createNegativeTargetingClauses
+     * @param array $params
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:03
+     */
+    public function createNegativeTargetingClauses(array $params)
+    {
+        return $this->httpPost('/sb/negativeTargets', $params, [], false);
+    }
+
+    /**
+     * getNegativeTargetingClause
+     *
+     * @param string $targetId
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:04
+     */
+    public function getNegativeTargetingClause(string $targetId)
+    {
+        return $this->httpGet('/sb/negativeTargets/' . $targetId, [], false);
+    }
+
+    /**
+     * archiveNegativeTargetingClauses
+     *
+     * @param string $targetId
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 16:04
+     */
+    public function archiveNegativeTargetingClauses(string $targetId)
+    {
+        return $this->httpDelete('/sb/negativeTargets/' . $targetId, [], [], false);
+    }
+
+    /**
+     * batchGetTargetingClauses
+     *
+     * @param array $targetIds
+     * @return array
+     *
+     * @author  baihe <b_aihe@163.com>
+     * @date    2020-02-18 15:49
+     */
+    public function batchGetNegativeTargetingClauses(array $targetIds)
+    {
+        return $this->httpPost('/sb/negativeTargets/batchGet', ['targetIds' => $targetIds], [], false);
+    }
+}
---
+++ src/SponsoredBrands/ProductTargeting/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace easyAmazonAdv\SponsoredBrands\ProductTargeting;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        $pimple['product_targeting'] = function ($app) {
+            return new Client($app);
+        };
+    }
+}
